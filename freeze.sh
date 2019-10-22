8802fb14c3a217c620b656673ef23ad08f5d606f
asabotie <asabotie@student.42.fr>
2019-10-22 11:53:46 +0200

    keep working on first test

--- freeze.sh
+++ freeze.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE}")"; pwd)"
+
 ##### BEG - GET GNL PROJECT PATH
 
-PROJECT_PATH_CONFIG_FILE=project_path.config
+PROJECT_PATH_CONFIG_FILE=${SCRIPT_PATH}/project_path.config
 
 if [ -f "${PROJECT_PATH_CONFIG_FILE}" ]
 then
@@ -10,22 +12,24 @@ then
 fi
 if [ ! -d "${PROJECT_PATH}" ]
 then
-	rm ${PROJECT_PATH_CONFIG_FILE}
+	rm ${PROJECT_PATH_CONFIG_FILE} &> /dev/null
 	while [ ! -d "${PROJECT_PATH}" ]
 	do
 		if [ "${PROJECT_PATH}" ]
 		then
 			echo ${PROJECT_PATH} directory not found.
 		fi
-		echo Type in your GNL project path. It will be stored in ./${PROJECT_PATH_CONFIG_FILE} file.
+		echo Type in your GNL project path. It will be stored in ${PROJECT_PATH_CONFIG_FILE} file.
 		echo  \(EXAMPLE: ../get_next_line\):
 		read PROJECT_PATH
+		PROJECT_PATH="$(cd "$(dirname "${PROJECT_PATH}")"; pwd)/$(basename "${PROJECT_PATH}")"
 	done
 	echo ${PROJECT_PATH} > ${PROJECT_PATH_CONFIG_FILE}
 fi
-PROJECT_PATH="$(cd "$(dirname "${PROJECT_PATH}")"; pwd)/$(basename "${PROJECT_PATH}")"
 
 ##### END - GET GNL PROJECT PATH
 
+make -C ${SCRIPT_PATH}/tests/test01 PATH="${PROJECT_PATH}"
+make clean -C ${SCRIPT_PATH}/tests/test01 PATH="${PROJECT_PATH}"
+${SCRIPT_PATH}/tests/test01/gnl_test
 
-make -C tests/test01 PATH="${PROJECT_PATH}"
--- tests/test01/main.c
+++ tests/test01/main.c
@@ -6,13 +6,28 @@
 /*   By: asabotie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/19 19:38:34 by asabotie          #+#    #+#             */
-/*   Updated: 2019/10/19 19:39:26 by asabotie         ###   ########.fr       */
+/*   Updated: 2019/10/22 11:19:38 by asabotie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "get_next_line.h"
 
 int	main(void)
 {
+	int	fd;
+	char	*line = NULL;
+
+	fd = open("file_to_read", O_RDONLY);
+	if (fd != -1)
+	{
+		while (get_next_line(fd, &line) == 1)
+		{
+			printf("%s\n", line);
+		}
+		close(fd);
+	}
 	return (0);
 }
