f910f0898d73692bf86434a76b478f603ce3ed4f
asabotie <asabotie@student.42.fr>
2019-10-28 13:25:03 +0100

    add description option

--- freeze.sh
+++ freeze.sh
@@ -4,7 +4,6 @@
 
 ### Add a timeout
 ### Add a flag to stop at first error or to diplay only first error
-### Add a flag to display a description for each test
 
 
 
@@ -73,6 +72,21 @@ fi
 
 MAIN_TEST_DIR=${SCRIPT_PATH}/tests
 
+display_test_description()
+{
+	if [ -f description ]
+	then
+		putstr_cyan " description:"; echo; putstr_magenta "$(cat -e description)"
+		echo -e "\n\n"
+	fi
+	putstr_cyan " buffer size:"; echo; putstr_magenta "$(cat -e buffer_size)"
+	echo -e "\n\n"
+	putstr_cyan " file to read:"; echo; putstr_magenta "$(cat -e file_to_read)"
+	echo -e "\n\n"
+	putstr_cyan " expected output:"; echo; putstr_magenta "$(cat -e expected_output)"
+	echo -e "\n\n"
+}
+
 ##### DEBUG OPTION #####
 
 if [ "${1}" = "-debug" -a "${2}" ]
@@ -85,6 +99,38 @@ then
 	make fclean PATH="${PROJECT_PATH}"
 	exit
 fi
+
+
+
+
+
+##### DESCRIPTION OPTION #####
+
+if [ "${1}" = "-description" -a "${2}" ]
+then
+	cd ${MAIN_TEST_DIR}/${2}
+	display_test_description
+	exit
+elif [ "${1}" = "-description" ]
+then
+	for TEST_DIR in ${MAIN_TEST_DIR}/*
+	do
+		echo -ne $(basename ${TEST_DIR}) "\n"
+		cd ${TEST_DIR}
+		display_test_description
+	done
+	exit
+fi
+
+
+##### UNKNOWN OPTION #####
+
+if [ "${1}" ]
+then
+	echo "unknomn options: " ${@}
+	exit
+fi
+
 	
 ##### RUN TESTS #####
 
@@ -105,17 +151,7 @@ do
 	then
 		putstr_red " KO"
 		echo -e "\n"
-		if [ -f description ]
-		then
-			putstr_cyan " description:"; echo; putstr_magenta "$(cat -e description)"
-			echo -e "\n\n"
-		fi
-		putstr_cyan " buffer size:"; echo; putstr_magenta "$(cat -e buffer_size)"
-		echo -e "\n\n"
-		putstr_cyan " file to read:"; echo; putstr_magenta "$(cat -e file_to_read)"
-		echo -e "\n\n"
-		putstr_cyan " expected output:"; echo; putstr_magenta "$(cat -e expected_output)"
-		echo -e "\n\n"
+		display_test_description
 		putstr_cyan " your output:"; echo; putstr_magenta "$(cat -e user_output)"
 		echo -e "\n\n"
 		if [ "${ERRORS}" ]
