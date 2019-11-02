96394c15e21f4f2bf388d919040019b4e24bc0a0
asabotie <asabotie@student.42.fr>
2019-11-02 12:32:21 +0100

    update test on stdin with correct behavior about gnl return value

--- stdin_test/main.c
+++ stdin_test/main.c
@@ -3,13 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asabotie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: asabotie <asabotie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/19 19:38:34 by asabotie          #+#    #+#             */
-/*   Updated: 2019/10/29 13:04:57 by asabotie         ###   ########.fr       */
+/*   Updated: 2019/11/02 12:29:28 by asabotie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -22,8 +23,12 @@ int	main(void)
 
 	while ((ret = get_next_line(0, &line)) == 1)
 	{
-		printf("Your get_next_line returned '%d' with the following line: '%s'\n", ret, line);
+		printf("%d|%s\n", ret, line);
+		free(line);
+		line = NULL;
 	}
-	printf("get_next_line return value: %d\n", ret);
+	printf("\n%d|%s\n", ret, line);
+	free(line);
+	line = NULL;
 	return (0);
 }
